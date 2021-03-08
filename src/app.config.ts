6c4ab2d437715a22f8c90d289e3f1a6586e7682a
huyyh <529386722@qq.com>
2021-03-08 22:22:25 +0800

    init

---
+++ src/app.config.ts
@@ -0,0 +1,13 @@
+export default {
+  pages: [
+    'pages/index/index',
+    'pages/itemize/index',
+    'pages/myInfo/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  }
+}
