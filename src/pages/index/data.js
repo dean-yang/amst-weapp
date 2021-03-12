813569470bbdbabc8359b48e8bcc83935f8f49c1
岳宇航 <yueyuhang@B-84CRJHD2-2204.local>
2021-03-12 17:52:46 +0800

    fixbug

--- src/pages/index/data.js
+++ src/pages/index/data.js
@@ -15,51 +15,51 @@ export const navDataList =
   [
     {
       image: 'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png',
-      value: '领取中心',
+      value: '家电维修',
       key:1
     },
     {
       image: 'https://img20.360buyimg.com/jdphoto/s72x72_jfs/t15151/308/1012305375/2300/536ee6ef/5a411466N040a074b.png',
-      value: '找折扣',
+      value: '家电安装',
       key:2
 
     },
     {
       image: 'https://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png',
-      value: '领会员',
+      value: '家电清洗',
       key:3
     },
     {
       image: 'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png',
-      value: '新品首发',
+      value: '灯泡维修',
       key:4
     },
     {
       image: 'https://img14.360buyimg.com/jdphoto/s72x72_jfs/t17251/336/1311038817/3177/72595a07/5ac44618Na1db7b09.png',
-      value: '领京豆',
+      value: '卫浴维修',
       key:5
     },
     {
       image: 'https://img30.360buyimg.com/jdphoto/s72x72_jfs/t5770/97/5184449507/2423/294d5f95/595c3b4dNbc6bc95d.png',
-      value: '手机馆',
+      value: '修锁换锁',
       key:6
     },{
       image: 'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png',
-      value: '新品首发',
+      value: '消毒杀菌',
       key:7
     },
     {
       image: 'https://img14.360buyimg.com/jdphoto/s72x72_jfs/t17251/336/1311038817/3177/72595a07/5ac44618Na1db7b09.png',
-      value: '领京豆',
+      value: '管道疏通',
       key:8
     },
     {
       image: 'https://img30.360buyimg.com/jdphoto/s72x72_jfs/t5770/97/5184449507/2423/294d5f95/595c3b4dNbc6bc95d.png',
-      value: '手机馆',
+      value: '防水补漏',
       key:9
     },{
       image: 'https://img30.360buyimg.com/jdphoto/s72x72_jfs/t5770/97/5184449507/2423/294d5f95/595c3b4dNbc6bc95d.png',
-      value: '手机馆',
+      value: '家具门窗',
       key:10
     },
   ]
