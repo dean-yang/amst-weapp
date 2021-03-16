10c1b4af2792780b4060cd98765be5b41366bde9
岳宇航 <yueyuhang@B-84CRJHD2-2204.local>
2021-03-16 12:27:06 +0800

    2020/3/16

--- src/pages/itemize/data.js
+++ src/pages/itemize/data.js
@@ -1,6 +1,56 @@
-import {navDataList} from '../index/data'
 
+export const navDataList = 
+  [
+    {
+      image: 'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png',
+      value: '洗衣机',
+      key:1
+    },
+    {
+      image: 'https://img20.360buyimg.com/jdphoto/s72x72_jfs/t15151/308/1012305375/2300/536ee6ef/5a411466N040a074b.png',
+      value: '冰箱',
+      key:2
 
+    },
+    {
+      image: 'https://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png',
+      value: '马桶',
+      key:3
+    },
+    {
+      image: 'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png',
+      value: 'x x x',
+      key:4
+    },
+    {
+      image: 'https://img14.360buyimg.com/jdphoto/s72x72_jfs/t17251/336/1311038817/3177/72595a07/5ac44618Na1db7b09.png',
+      value: 'xxx',
+      key:5
+    },
+    {
+      image: 'https://img30.360buyimg.com/jdphoto/s72x72_jfs/t5770/97/5184449507/2423/294d5f95/595c3b4dNbc6bc95d.png',
+      value: 'xxx',
+      key:6
+    },{
+      image: 'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png',
+      value: 'xxx',
+      key:7
+    },
+    {
+      image: 'https://img14.360buyimg.com/jdphoto/s72x72_jfs/t17251/336/1311038817/3177/72595a07/5ac44618Na1db7b09.png',
+      value: 'xxx',
+      key:8
+    },
+    {
+      image: 'https://img30.360buyimg.com/jdphoto/s72x72_jfs/t5770/97/5184449507/2423/294d5f95/595c3b4dNbc6bc95d.png',
+      value: 'xxx',
+      key:9
+    },{
+      image: 'https://img30.360buyimg.com/jdphoto/s72x72_jfs/t5770/97/5184449507/2423/294d5f95/595c3b4dNbc6bc95d.png',
+      value: 'xxx',
+      key:10
+    },
+  ]
 export const tabList = [
     { title:"家电维修",id:0},
    { title:"家电安装",id:1},
