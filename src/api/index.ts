f7b795b5afc6b1377683552c19e747899e6fbeda
岳宇航 <yueyuhang@B-84CRJHD2-2204.local>
2021-03-17 18:12:00 +0800

    2020/3/17 18:00

---
+++ src/api/index.ts
@@ -0,0 +1,3 @@
+export const getFirstClass = '/api/firstClassification/get' // 获取一级分类
+export const getSecondPro = '/api/secondClassification/get' // 获取二级产品
+export const getBanner = '/api/banner/get'  // 获取banner图片
